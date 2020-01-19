36d424b2818eff32511efb5b9c86d5c385960257
komatsu <ryotak3@gmail.com>
2020-01-19 14:13:42 +0900

    firebaseのローカル設定を登録

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
-// Replace this section with the config file of your own Firebase project. 
-const firebaseConfig = {
-  apiKey: "AIzaSyDIRRdPkeCQVniv22YDXH6oA1oW7AOPwI4",
-  authDomain: "firepresso.firebaseapp.com",
-  databaseURL: "https://firepresso.firebaseio.com",
-  projectId: "firepresso",
-  storageBucket: "firepresso.appspot.com",
-  messagingSenderId: "351087874664",
-  appId: "1:351087874664:web:ad9ecba660715261e861dd"
+// Replace this section with the config file of your own Firebase project.
+var firebaseConfig = {
+    apiKey: "AIzaSyD_JMf8-H5FoAtelpLRrB5tEvC73_yISxI",
+    authDomain: "firepresso-a8462.firebaseapp.com",
+    databaseURL: "https://firepresso-a8462.firebaseio.com",
+    projectId: "firepresso-a8462",
+    storageBucket: "firepresso-a8462.appspot.com",
+    messagingSenderId: "650613413408",
+    appId: "1:650613413408:web:a4f9812f23ec69b804ce45"
 };
 
 export default firebaseConfig;
